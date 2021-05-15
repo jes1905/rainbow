796bd8b7a849707dd326678da97e4d0b92cf365c
jes1905 <jadesanche2005@gmail.com>
2021-05-15 13:57:00 -0400

    final project

--- scheduler/app.py
+++ scheduler/app.py
@@ -6,6 +6,8 @@ from flask_apscheduler import APScheduler
 import sys
 import sqlite3
 
+
+
 app = Flask(__name__)
 scheduler = APScheduler()
 scheduler.init_app(app)
@@ -13,11 +15,9 @@ scheduler.start()
 sense = SenseHat()
 
 
+
 @app.route('/', methods = ["GET", "POST"])
 def index():
-    task = request.form['task']
-    date = request.form['date']
-    rowid = request.form['rowid']
     conn = sqlite3.connect('./static/data/tasks.db')
     curs = conn.cursor()
     tasks = []
@@ -62,22 +62,21 @@ def all():
 
 @app.route('/delete', methods = ['GET', 'POST'])
 def delete():
-    task = request.form['task']
-    date = request.form['date']
     conn = sqlite3.connect('./static/data/tasks.db')
     curs = conn.cursor()
-    curs.execute("DELETE FROM tasks WHERE task = 'task' ",(task,date))
+    curs.execute("DELETE FROM tasks WHERE task = (?)",(task))
     conn.commit()
     conn.close()
     conn = sqlite3.connect('./static/data/tasks.db')
     curs = conn.cursor()
     tasks = []
     rows = curs.execute("SELECT * from tasks")
+    
     for row in rows:
         task = {'task':row[0], 'date':row[1]}
         tasks.pop(task)
     conn.close()
-    return render_template('all.html', task = task, date = date, tasks = tasks)
+    return render_template('all.html', tasks = tasks)
 
 
 if __name__ == '__main__':
